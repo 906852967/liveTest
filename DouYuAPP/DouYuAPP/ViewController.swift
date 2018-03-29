00db434771f855eff34ee54c852ab40fab423b19
globalwings <globalwings@globalwingsdeimac.local>
2018-03-29 17:02:26 +0800

    删除测试地图

--- DouYuAPP/DouYuAPP/ViewController.swift
+++ DouYuAPP/DouYuAPP/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ DouYuAPP/DouYuAPPTests/DouYuBridge.h
@@ -0,0 +1,11 @@
+//
+//  DouYuBridge.h
+//  DouYuAPP
+//
+//  Created by globalwings  on 2018/3/28.
+//  Copyright © 2018年 globalwings . All rights reserved.
+//
+
+#ifndef DouYuBridge_h
+#define DouYuBridge_h
+#endif
