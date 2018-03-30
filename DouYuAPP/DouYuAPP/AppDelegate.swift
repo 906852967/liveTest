40cdf12f9e91ee0ea0165436c57f7d31d38a6346
globalwings <globalwings@globalwingsdeimac.local>
2018-03-30 10:16:46 +0800

    项目基本设置

--- DouYuAPP/DouYuAPP/AppDelegate.swift
+++ DouYuAPP/DouYuAPP/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -40,4 +39,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
 
