ac15d4b25f12697f9b5e4cc5ff4f86d21337a16c
globalwings <globalwings@globalwingsdeimac.local>
2018-04-18 17:28:16 +0800

    Tabbar修改

--- DouYuAPP/DouYuAPP/AppDelegate.swift
+++ DouYuAPP/DouYuAPP/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().tintColor = UIColor.orange
         return true
     }
     func applicationWillResignActive(_ application: UIApplication) {
