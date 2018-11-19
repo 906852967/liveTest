30542d0b9130ddb65d7f709c4227db87a9567160
姜有伟 <jiangyouwei@jiangyouweidemacbook-pro.local>
2018-11-20 00:01:01 +0800

    策略模式结合简单工厂模式

--- 设计模式/设计模式/ViewController.swift
+++ 设计模式/设计模式/ViewController.swift
@@ -17,12 +17,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
     @IBAction func totalAction(_ sender: UIButton) {
         totalPeiceLabel.text = String(btnOk_click("打八折"))
     }
     
 }
+
 extension ViewController{
     func btnOk_click(_ selectedString : String) -> Double{
         var totalPrice : Double = 0
@@ -41,13 +41,72 @@ extension ViewController{
     }
 }
 
-
 //现金收费抽象类
 class CashSuper: NSObject {
-    public func acceptCash(_ money : Double){
-        
+    public func acceptCash(_ money : Double) -> Double{
+        return 0.0
     }
 }
 
 //正常收费类
+class CashNormal: CashSuper {
+    override func acceptCash(_ money: Double) -> Double{
+        return money
+    }
+}
+//打折收费子类
+class CashRebate: CashSuper {
+    var moneyRebate = 0.0
+    init(_ moneyRebateString : String){
+        moneyRebate = (moneyRebateString as NSString).doubleValue
+    }
+    override func acceptCash(_ money: Double) -> Double {
+        return money * moneyRebate
+    }
+}
+//返利收费子类
+class CashReturn : CashSuper {
+    var moneyCondition = 0.0
+    var moneyReturn = 0.0
+    init(moneyConditionString : String, moneyReturnString : String ) {
+        moneyReturn = (moneyReturnString as NSString).doubleValue
+        moneyCondition = (moneyConditionString as NSString).doubleValue
+    }
+    override func acceptCash(_ money: Double) -> Double {
+        var result = 0.0
+        if money >= moneyCondition{
+            result = money - floor(money / moneyCondition) * moneyReturn
+        }
+        return result
+    }
+}
+//现金收费子类
+class CashFactory : NSObject {
+    func createCashAccept(_ type : String) -> CashSuper{
+        var cs : CashSuper
+        switch type {
+        case "正常收费":
+            cs = CashNormal()
+        case "满300返100":
+            cs = CashReturn(moneyConditionString: "300", moneyReturnString: "100")
+        case "打8折":
+            cs = CashRebate("0.8")
+        default:
+            return CashSuper()
+        }
+        return cs
+    }
+}
+//Context
+class CashContext : NSObject {
+//    var cs : CashSuper
+//    var type : String = ""
+//    init(_ cashSuper : CashSuper) {
+//       cs = cashSuper
+//    }
+//    func contextsInterfance() -> CashSuper{
+//        return cashSuper
+//    }
+}
+
 
