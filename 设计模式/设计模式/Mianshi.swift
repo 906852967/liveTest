99c050bd4f65e02056d0b085ac598c4dae4fa6bf
姜有伟 <jiangyouwei@jiangyouweidemacbook-pro.local>
2018-11-26 23:56:54 +0800

    新增

--- 设计模式/设计模式/Mianshi.swift
+++ 设计模式/设计模式/Mianshi.swift
@@ -12,7 +12,7 @@ class Mianshi: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        test()
     }
 }
 extension Mianshi{
@@ -39,5 +39,37 @@ extension Mianshi{
     }
     
     //字符串反转
+    func _reverse<T>(_ chars : inout [T], _ start : Int, _ end : Int){
+        var start = start, end = end
+        while start < end {
+            _swap(&chars, start, end)
+            start += 1
+            end -= 1
+        }
+    }
+    func _swap<T>(_ chars : inout [T], _ p : Int, _ q : Int){
+        (chars[p], chars[q]) = (chars[q], chars[p])
+    }
+    
+    func reverseWords(s : String?) -> String?{
+        guard let s = s else{
+            return nil
+        }
+        var chars = Array(arrayLiteral: s.characters), start = 0
+        _reverse(&chars, 0, chars.count - 1)
+        for i in 0..<chars.count{
+            if i == chars.count - 1 || chars[i + 1] as? String == " " {
+                _reverse(&chars, start, i)
+                start = i + 2
+            }
+        }
+        return String(chars.description)
+    }
+    func test(){
+        var string = "the sky is blue"
+        
+        let reserveString = reverseWords(s: string)
+        print(reserveString)
+    }
         
 }
